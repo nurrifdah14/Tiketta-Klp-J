24b756ce7c367b65770b2356d940c6a68c798be1
DESKTOP-5JO9TMG\Rifdah <60900119009@uin-alauddin.ac.id>
2021-12-27 11:12:37 +0800

    progres 3

---
+++ app/src/main/java/com/example/tiketta/JadwalFragment.kt
@@ -0,0 +1,24 @@
+package com.example.tiketta
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tiketta.databinding.FragmentHomeBinding
+import com.example.tiketta.databinding.FragmentJadwalBinding
+
+class JadwalFragment : Fragment() {
+    private lateinit var _binding: FragmentJadwalBinding
+    private val binding get()=_binding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= FragmentJadwalBinding.inflate(inflater)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
